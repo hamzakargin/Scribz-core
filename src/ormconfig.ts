02806af62d0799ec28893d9991a8dca7b43e27ac
hamzakargin <hamzakargin@outlook.com>
2025-03-28 18:35:10 +0300

    DeployProd2

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,12 +1,15 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: PostgresConnectionOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'scribzcore',
-  password: '123',
-  database: 'scribzcore',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: false,
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
